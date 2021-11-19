527b4ec28ce55636b50d796c723d5c9fe74f9b38
Todd <todd.thomas@lhswildcats.org>
2021-11-19 09:16:45 -0600

    created working timer

---
+++ FinalTimerHopefully/ViewController.swift
@@ -0,0 +1,61 @@
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var pauseButton: UIButton!
+    
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var addSeconds: UIButton!
+    @IBOutlet weak var minusSeconds: UIButton!
+    var timer = Timer()
+    var seconds = 60
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+    @IBAction func startButton(_ sender: Any)
+    {
+        timer.invalidate()
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.timerClass), userInfo: nil, repeats: true)
+    }
+    @IBAction func pauseButton(_ sender: Any)
+    {
+        timer.invalidate()
+    }
+    @IBAction func resetButton(_ sender: Any)
+    {
+        timer.invalidate()
+        seconds = 60
+        timerLabel.text = String(seconds)
+    }
+    @IBAction func addSeconds(_ sender: Any)
+    {
+        seconds = seconds + 5
+        timerLabel.text = String(seconds)
+    }
+    
+    @IBAction func minusSeconds(_ sender: Any)
+    {
+    seconds = seconds - 5
+    timerLabel.text = String(seconds)
+    }
+    @objc func timerClass()
+    {
+seconds -= 1
+timerLabel.text = String(seconds)
+        
+if(seconds == 0)
+{
+    timer.invalidate()
+}
+
+    }
+}
+
+
+
